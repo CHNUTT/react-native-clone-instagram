48ffc237fe6b71fef3c5982cd61c141e0ecbf6b6
CHNUTT <chokwittaya.nutt@gmail.com>
2021-06-02 19:18:26 -0500

    [ React Navigation Native ] - implement react nagivation to navigate between screens

---
+++ app/screens/Landing/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  button: {},
+});
+
+export default styles;
