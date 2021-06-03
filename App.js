6735f5aa54aab7c443caa958e9fcfc64021dd601
CHNUTT <chokwittaya.nutt@gmail.com>
2021-06-02 22:45:50 -0500

    [ Screens - Register ] - implement register logic and basic layout

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet } from 'react-native';
+
+import * as firebase from 'firebase';
+import { firebaseConfig } from './app/configs/firebase';
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 import LandingScreen from './app/screens/Landing';
-import { StyleSheet } from 'react-native';
+import RegisterScreen from './app/screens/Register';
 
 const Stack = createStackNavigator();
 
@@ -16,6 +25,7 @@ export default function App() {
           component={LandingScreen}
           options={{ headerShown: false }}
         />
+        <Stack.Screen name='Register' component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ app/screens/Register/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: 'center' },
+  input: {
+    width: '80%',
+    height: 30,
+    marginTop: 10,
+    borderRadius: 5,
+    borderColor: 'black',
+    borderWidth: 1,
+    paddingLeft: 10,
+  },
+});
+
+export default styles;
